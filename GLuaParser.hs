957754649c91337096a14417919d0b1fb6ca0259
Falco Peijnenburg <fpeijnenburg@gmail.com>
2014-07-10 13:17:29 +0200

    Simple lexer tester

--- GLuaParser.hs
+++ GLuaParser.hs
@@ -1,2 +1,15 @@
 module GLuaParser where
 
+import Lexer
+import TokenTypes
+import System.FilePath
+import System.Environment
+import System.IO
+
+main = do
+	args <- getArgs
+	let file = head args
+	contents <- readFile file
+	let lex = alexScanTokens contents
+
+	print . show $ lex
