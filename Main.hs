25d4b73e1bc9e5ea6d80fede7c7362235f3cd55b
Falco Peijnenburg <fpeijnenburg@gmail.com>
2014-07-10 13:20:13 +0200

    Main.hs

--- Main.hs
+++ Main.hs
@@ -1,7 +1,15 @@
 module Main where
 
+import Lexer
+import TokenTypes
+import System.FilePath
+import System.Environment
 import System.IO
 
 main = do
 	args <- getArgs
-	return ()
+	let file = head args
+	contents <- readFile file
+	let lex = alexScanTokens contents
+
+	print . show $ lex
