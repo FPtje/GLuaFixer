ee04aca994c96b1ea00619c72c5c13c6411ee363
Falco Peijnenburg <fpeijnenburg@gmail.com>
2014-07-12 14:53:54 +0200

    Test case: check whether printed lexed files are equal to the original

--- Main.hs
+++ Main.hs
@@ -5,11 +5,18 @@ import TokenTypes
 import System.FilePath
 import System.Environment
 import System.IO
+import Control.Monad
 
 main = do
-	args <- getArgs
-	let file = head args
-	contents <- readFile file
-	let lex = execParseTokens contents
+    args <- getArgs
+    let file = head args
+    contents <- readFile file
+    let lex = execParseTokens contents
+    let newcontents = concatMap show $ fst lex
+    --putStrLn newcontents
 
-	putStrLn . show . snd $ lex
+    unless (null $ snd lex) $
+        print . snd $ lex
+
+    when (newcontents /= contents) $
+        putStrLn $ "NOT the same! " ++ file
