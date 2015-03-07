9578f785242879a7e22b12a395d64f11d5574282
FPtje <fpeijnenburg@gmail.com>
2015-03-07 12:02:54 +0100

    More optimisations

--- src/GLua/Parser.hs
+++ src/GLua/Parser.hs
@@ -60,13 +60,7 @@ parseNameList = (:) <$> pName <*> pMany (pMTok Comma *> pName)
 
 -- | Parse list of function parameters
 parseParList :: AParser [MToken]
-parseParList = pName <**> (
-                    pMTok Comma <**> (
-                        (\a _ c -> [c, a]) <$> pMTok VarArg <<|>
-                        (\a _ c -> c : a)  <$> parseParList
-                    ) <<|>
-                    pReturn (: [])
-               ) <<|> pReturn []
+parseParList = (flip (++)) <$> pListSep (pMTok Comma) pName <*> pAtMost 1 (pMTok VarArg)
 
 -- | Parses the full AST
 -- Its first parameter contains all comments
