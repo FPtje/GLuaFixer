4b340e3547776afe9698cd719e5187fe9bdf7380
FPtje <fpeijnenburg@gmail.com>
2015-07-17 21:02:26 +0200

    Simpler main for now

--- src/GLuaFixer/Main.hs
+++ src/GLuaFixer/Main.hs
@@ -3,32 +3,12 @@ module Main where
 import GLua.Lexer
 import GLua.Parser
 import GLua.AG.PrettyPrint
-
-import Data.Char
-
-import System.Environment
 import System.Exit
 import Control.Monad
 
-help :: String
-help = unlines ["",
- "Usage: gluafixer <FILE>"
- ]
-
-
 main :: IO ()
 main = do
-    args <- getArgs
-
-    -- Argument checking
-    when (length args < 2) $ do
-        putStrLn help
-        exitSuccess
-
-    let action = head args
-    let file = args !! 1
-
-    contents <- readFile file
+    contents <- getContents
 
     -- Lex the file
     let lexed = execParseTokens contents
@@ -36,13 +16,8 @@ main = do
     let errors = snd lexed
 
     -- Print any lexing errors
-    unless (null errors) $ do
+    when (not . null $ errors) $ do
         mapM_ print errors
-        -- Attempt to fix errors when asked
-        when (map toLower action == "fix") $ do
-            writeFile file . concatMap show $ tokens
-            putStrLn "Success"
-            exitSuccess
 
         exitWith (ExitFailure 1)
 
