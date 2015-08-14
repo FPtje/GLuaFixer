1d69742703836e79e80d494765b776db20dc4527
FPtje <fpeijnenburg@gmail.com>
2015-08-14 20:48:22 +0200

    Basics for a linter

---
+++ src/GLuaFixer/LintMain.hs
@@ -0,0 +1,35 @@
+module Main where
+
+import System.Environment
+import System.IO
+import GLua.Lexer
+
+-- | Read file in utf8_bom because that seems to work better
+doReadFile :: FilePath -> IO String
+doReadFile f = do
+    handle <- openFile f ReadMode
+    hSetEncoding handle utf8_bom
+    contents <- hGetContents handle
+    return contents
+
+
+-- | Lint a set of files
+lint :: [FilePath] -> IO ()
+lint [] = return ()
+lint (f : fs) = do
+    contents <- doReadFile f
+    let lexed = execParseTokens contents
+
+    -- TODO: lint lexed
+    let warnings = const [] lexed
+
+    -- Print all warnings
+    mapM_ putStrLn warnings
+
+    -- Lint the other files
+    lint fs
+
+main :: IO ()
+main = do
+    args <- getArgs
+    lint args
