98f250bdec1fa538b37eeb2c36b146bf6a4b6cb4
FPtje <fpeijnenburg@gmail.com>
2015-08-18 19:04:50 +0200

    Normal tabs to normal spaces

--- src/GLuaFixer/LintMain.hs
+++ src/GLuaFixer/LintMain.hs
@@ -28,7 +28,7 @@ lint [] = return ()
 lint (f : fs) = do
     contents <- doReadFile f
 
-    let tabsToSpaces = replace "\n\t" "\n " contents
+    let tabsToSpaces = replace "\t" " " contents
 
     let lexed = execParseTokens tabsToSpaces
     let tokens = fst lexed
