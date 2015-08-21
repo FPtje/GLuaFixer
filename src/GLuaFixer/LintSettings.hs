693fae4ccaf306977b25e837db8ca101ac334cb4
FPtje <fpeijnenburg@gmail.com>
2015-08-21 16:34:52 +0200

    Initial work for settings

---
+++ src/GLuaFixer/LintSettings.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+module GLuaFixer.LintSettings where
+
+import Data.Aeson
+import Control.Monad
+
+data LintSettings = LintSettings {
+                        maxScopeDepth :: Int
+                    }
+
+defaultLintSettings :: LintSettings
+defaultLintSettings =   LintSettings {
+                            maxScopeDepth = 7
+                        }
+
+instance FromJSON LintSettings where
+    parseJSON (Object v) = LintSettings <$>
+                               v .:? "maxScopeDepth" .!= (maxScopeDepth defaultLintSettings)
+
+    parseJSON _          = mzero
+
+instance ToJSON LintSettings where
+    toJSON ls = object [
+                        "maxScopeDepth" .= maxScopeDepth ls
+                       ]
