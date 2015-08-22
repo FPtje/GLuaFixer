d2cbf8089b8b0d8975617f4c036b288bfb279f02
FPtje <fpeijnenburg@gmail.com>
2015-08-22 12:10:18 +0200

    Setting to report syntax inconsistencies

--- src/GLuaFixer/LintSettings.hs
+++ src/GLuaFixer/LintSettings.hs
@@ -6,24 +6,28 @@ import Control.Monad
 
 data LintSettings = LintSettings {
                         maxScopeDepth :: Int,
-                        reportSyntaxErrors :: Bool
+                        reportSyntaxErrors :: Bool,
+                        reportSyntaxInconsistencies :: Bool
                     } deriving (Show)
 
 defaultLintSettings :: LintSettings
 defaultLintSettings =   LintSettings {
                             maxScopeDepth = 7,
-                            reportSyntaxErrors = False
+                            reportSyntaxErrors = False,
+                            reportSyntaxInconsistencies = True
                         }
 
 instance FromJSON LintSettings where
     parseJSON (Object v) = LintSettings <$>
                                v .:? "maxScopeDepth" .!= (maxScopeDepth defaultLintSettings) <*>
-                               v .:? "reportSyntaxErrors" .!= (reportSyntaxErrors defaultLintSettings)
+                               v .:? "reportSyntaxErrors" .!= (reportSyntaxErrors defaultLintSettings) <*>
+                               v .:? "reportSyntaxInconsistencies" .!= (reportSyntaxInconsistencies defaultLintSettings)
 
     parseJSON _          = mzero
 
 instance ToJSON LintSettings where
     toJSON ls = object [
                         "maxScopeDepth" .= maxScopeDepth ls,
-                        "reportSyntaxErrors" .= reportSyntaxErrors ls
+                        "reportSyntaxErrors" .= reportSyntaxErrors ls,
+                        "reportSyntaxInconsistencies" .= reportSyntaxInconsistencies ls
                        ]
