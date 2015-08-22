2c2223c5589a310f067e86c61b91e14df21c7c66
FPtje <fpeijnenburg@gmail.com>
2015-08-22 12:53:29 +0200

    Config options for all the lexed linter things

--- src/GLuaFixer/LintSettings.hs
+++ src/GLuaFixer/LintSettings.hs
@@ -7,21 +7,30 @@ import Control.Monad
 data LintSettings = LintSettings {
                         maxScopeDepth :: Int,
                         reportSyntaxErrors :: Bool,
-                        reportSyntaxInconsistencies :: Bool
+                        reportSyntaxInconsistencies :: Bool,
+                        reportDeprecated :: Bool,
+                        reportWhitespaceStyle :: Bool,
+                        reportBeginnerMistakes :: Bool
                     } deriving (Show)
 
 defaultLintSettings :: LintSettings
 defaultLintSettings =   LintSettings {
                             maxScopeDepth = 7,
                             reportSyntaxErrors = False,
-                            reportSyntaxInconsistencies = True
+                            reportSyntaxInconsistencies = True,
+                            reportDeprecated = True,
+                            reportWhitespaceStyle = True,
+                            reportBeginnerMistakes = True
                         }
 
 instance FromJSON LintSettings where
     parseJSON (Object v) = LintSettings <$>
                                v .:? "maxScopeDepth" .!= (maxScopeDepth defaultLintSettings) <*>
                                v .:? "reportSyntaxErrors" .!= (reportSyntaxErrors defaultLintSettings) <*>
-                               v .:? "reportSyntaxInconsistencies" .!= (reportSyntaxInconsistencies defaultLintSettings)
+                               v .:? "reportSyntaxInconsistencies" .!= (reportSyntaxInconsistencies defaultLintSettings) <*>
+                               v .:? "reportDeprecated" .!= (reportDeprecated defaultLintSettings) <*>
+                               v .:? "reportWhitespaceStyle" .!= (reportWhitespaceStyle defaultLintSettings) <*>
+                               v .:? "reportBeginnerMistakes" .!= (reportBeginnerMistakes defaultLintSettings)
 
     parseJSON _          = mzero
 
@@ -29,5 +38,8 @@ instance ToJSON LintSettings where
     toJSON ls = object [
                         "maxScopeDepth" .= maxScopeDepth ls,
                         "reportSyntaxErrors" .= reportSyntaxErrors ls,
-                        "reportSyntaxInconsistencies" .= reportSyntaxInconsistencies ls
+                        "reportSyntaxInconsistencies" .= reportSyntaxInconsistencies ls,
+                        "reportDeprecated" .= reportDeprecated ls,
+                        "reportWhitespaceStyle" .= reportWhitespaceStyle ls,
+                        "reportBeginnerMistakes" .= reportBeginnerMistakes ls
                        ]
