598db6f1504e887d1da9c2339e3ab276bb32f61c
FPtje <fpeijnenburg@gmail.com>
2015-11-29 20:45:40 +0100

    1.6.3

--- src/GLuaFixer/LintMain.hs
+++ src/GLuaFixer/LintMain.hs
@@ -21,7 +21,7 @@ import Control.Applicative
 import GLuaFixer.AG.DarkRPRewrite
 
 version :: String
-version = "1.6.2"
+version = "1.6.3"
 
 -- | Read file in utf8_bom because that seems to work better
 doReadFile :: FilePath -> IO String
