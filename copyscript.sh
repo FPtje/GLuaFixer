87e94ec659a486fb932e32bb13261b741e6927c3
Falco Peijnenburg <falco@lumiguide.nl>
2016-10-23 19:50:04 +0200

    Copyscript update

--- copyscript.sh
+++ copyscript.sh
@@ -1,10 +1,8 @@
-#!/usr/bin/env /bin/bash
-
 # The point of placing copies of these generated files near their source codes is making ghci and the linter work
 # in files that include them.
 rm src/GLua/AG/*.hs
 rm src/GLuaFixer/AG/*.hs
-cp dist/build/glualint/glualint-tmp/GLua/AG/*.hs src/GLua/AG/
-cp dist/build/glualint/glualint-tmp/GLuaFixer/AG/*.hs src/GLuaFixer/AG/
-cp dist/build/glualint/glualint-tmp/GLuaFixer/AG/LexLint.hs src/GLuaFixer/AG/
-cp dist/build/glualint/glualint-tmp/GLuaFixer/AG/ASTLint.hs src/GLuaFixer/AG/
+cp dist/build/GLua/AG/*.hs src/GLua/AG/
+cp dist/build/GLuaFixer/AG/*.hs src/GLuaFixer/AG/
+cp dist/build/GLuaFixer/AG/LexLint.hs src/GLuaFixer/AG/
+cp dist/build/GLuaFixer/AG/ASTLint.hs src/GLuaFixer/AG/
