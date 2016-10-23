e10fccccf5ea1c0993676c233dc0cefead68ec47
Falco Peijnenburg <falco@lumiguide.nl>
2016-10-23 12:10:36 +0200

    Optionally pin down nixpkgs

--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
-{pkgs ? import <nixpkgs> {}, stdenv ? pkgs.stdenv}:
+{pkgs ? import <nixpkgs> {}, stdenv ? pkgs.stdenv, pinNixDown ? true}:
 let
-  nixpkgs = import (pkgs.fetchgit {
+  # Pin nixpkgs version down by default, but allow building with another version
+  nixpkgs = if !pinNixDown then pkgs else import (pkgs.fetchgit {
     url = "https://github.com/NixOS/nixpkgs.git";
     rev = "af0fec6d0a3e28c815e38296f3758e7d0916eba9";
     sha256 = "0npkwybsh3v5jmjwp27xg47s0467msqilpiq2wikrmnrgb86ynpc";
@@ -18,7 +19,7 @@ mkDerivation {
     ListLike MissingH mtl parsec pretty uu-parsinglib uuagc uuagc-cabal
     vector
   ];
-  executableHaskellDepends = [ base directory ];
+  executableHaskellDepends = [ base directory uuagc uuagc-cabal ];
   homepage = "https://github.com/FPtje/GLuaFixer";
   description = "Attempts to fix your syntax erroring Lua files";
   license = stdenv.lib.licenses.lgpl21;
