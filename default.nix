8a343b722840352cd0979ebdccc2616f8361d623
Falco Peijnenburg <falco@lumiguide.nl>
2016-10-23 10:06:01 +0200

    Pin down nixpkgs version in default.nix

--- default.nix
+++ default.nix
@@ -1,7 +1,12 @@
-{ mkDerivation, aeson, array, base, bytestring, containers
-, directory, filemanip, filepath, ListLike, MissingH, mtl, parsec
-, pretty, stdenv, uu-parsinglib, uuagc, uuagc-cabal, vector
-}:
+{pkgs ? import <nixpkgs> {}, stdenv ? pkgs.stdenv}:
+let
+  nixpkgs = import (pkgs.fetchgit {
+    url = "https://github.com/NixOS/nixpkgs.git";
+    rev = "af0fec6d0a3e28c815e38296f3758e7d0916eba9";
+    sha256 = "0npkwybsh3v5jmjwp27xg47s0467msqilpiq2wikrmnrgb86ynpc";
+  }) {};
+in
+with nixpkgs.haskellPackages;
 mkDerivation {
   pname = "glualint-lib";
   version = "0.1.0.0";
