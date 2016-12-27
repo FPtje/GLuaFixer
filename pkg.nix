ba80c035fc3f6906079dce967641b9b950db75a9
Falco Peijnenburg <falco@lumiguide.nl>
2016-12-27 10:13:32 +0100

    Some pkg.nix changes

--- pkg.nix
+++ pkg.nix
@@ -1,10 +1,10 @@
-{pkgs ? import <nixpkgs> {}, stdenv ? pkgs.stdenv, pinNixDown ? true}:
+{pkgs ? import <nixpkgs> {}, stdenv ? pkgs.stdenv, pinNixDown ? false}:
 let
   # Pin nixpkgs version down by default, but allow building with another version
   nixpkgs = if !pinNixDown then pkgs else import (pkgs.fetchgit {
     url = "https://github.com/NixOS/nixpkgs.git";
     rev = "af0fec6d0a3e28c815e38296f3758e7d0916eba9";
-    sha256 = "0npkwybsh3v5jmjwp27xg47s0467msqilpiq2wikrmnrgb86ynpc";
+    sha256 = "0knbmva5bmilhz4w3xi55dg22m7g44viawxa5n5x228av3bcmy5i";
   }) {};
 
   drv = nixpkgs.haskellPackages.callPackage ./. {};
