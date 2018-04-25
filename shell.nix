ef667237ff5970631d415442f751210808f44030
Falco Peijnenburg <fpeijnenburg@gmail.com>
2018-04-25 21:15:53 +0200

    use fetchFromGitHub

--- shell.nix
+++ shell.nix
@@ -1,8 +1,9 @@
 {pkgs ? import <nixpkgs> {}, stdenv ? pkgs.stdenv, pinNixDown ? true}:
 let
   # Pin nixpkgs version down by default, but allow building with another version
-  nixpkgs = if !pinNixDown then pkgs else import (pkgs.fetchgit {
-    url = "https://github.com/NixOS/nixpkgs.git";
+  nixpkgs = if !pinNixDown then pkgs else import (pkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
     rev = "af0fec6d0a3e28c815e38296f3758e7d0916eba9";
     sha256 = "0knbmva5bmilhz4w3xi55dg22m7g44viawxa5n5x228av3bcmy5i";
   }) {};
