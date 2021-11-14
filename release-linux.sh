fc2b3645605c9489ca7e1fa70049bc2872338564
Falco Peijnenburg <falco.peijnenburg@channable.com>
2021-11-14 16:17:47 +0100

    Update release-linux to use nix develop

--- release-linux.sh
+++ release-linux.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 set -o errexit \
     -o nounset \
     -o pipefail
 
 cabal clean
 
-nix-shell --run "cabal v1-build"
+nix develop --command bash -c "./AGGenerator.sh && cabal v1-clean && cabal v1-build"
 
 VERSION=$(dist/build/glualint/glualint --version)
 echo "Packing glualint version $VERSION"
@@ -12,7 +13,7 @@ echo "Packing glualint version $VERSION"
 cp dist/build/glualint/glualint .
 
 # See https://github.com/FPtje/GLuaFixer/issues/54
-nix-shell -p patchelf --run "patchelf --set-interpreter /lib64/ld-linux-x86-64.so.2 glualint"
+nix develop --command patchelf --set-interpreter /lib64/ld-linux-x86-64.so.2 glualint
 
 zip "glualint-$VERSION-linux.zip" glualint
 
