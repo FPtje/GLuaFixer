f3c1089c95d06e1f3cc01d9bd6a9d7274aae3472
Falco Peijnenburg <falco.peijnenburg@channable.com>
2022-03-13 13:38:34 +0100

    Modify release-linux script for static bulid

--- release-linux.sh
+++ release-linux.sh
@@ -3,22 +3,13 @@ set -o errexit \
     -o nounset \
     -o pipefail
 
-cabal clean
+nix build
 
-nix develop --command bash -c "./AGGenerator.sh && cabal v1-clean && cabal v1-build"
-
-VERSION=$(dist/build/glualint/glualint --version)
+VERSION=$(result/bin/glualint --version)
 echo "Packing glualint version $VERSION"
 
-cp dist/build/glualint/glualint .
-
-# See https://github.com/FPtje/GLuaFixer/issues/54
-nix develop --command patchelf --set-interpreter /lib64/ld-linux-x86-64.so.2 glualint
+cp result/bin/glualint .
 
 zip "glualint-$VERSION-linux.zip" glualint
 
-strip glualint
-
-zip "glualint-$VERSION-linux-stripped.zip" glualint
-
-rm glualint
+rm -f glualint
