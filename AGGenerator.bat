a40b26f6ab3e426c4c06b53f0d39461cffbd0490
Falco Peijnenburg <fpeijnenburg@gmail.com>
2023-03-18 22:28:37 +0100

    Fix AGGenerator with latest cabal

--- AGGenerator.bat
+++ AGGenerator.bat
@@ -1,5 +1,5 @@
-.cabal-sandbox\bin\uuagc.exe --haskellsyntax --data src/GLua/AG/AST.ag
-.cabal-sandbox\bin\uuagc.exe --haskellsyntax --data --strictdata src/GLua/AG/Token.ag
-.cabal-sandbox\bin\uuagc.exe --catas --haskellsyntax --semfuns --wrappers --signatures src/GLua/AG/PrettyPrint.ag
-.cabal-sandbox\bin\uuagc.exe --catas --haskellsyntax --semfuns --wrappers --signatures --optimize src/GLuaFixer/AG/LexLint.ag
-.cabal-sandbox\bin\uuagc.exe --catas --haskellsyntax --semfuns --wrappers --signatures --optimize src/GLuaFixer/AG/ASTLint.ag
+cabal exec -- uuagc --haskellsyntax --data src/GLua/AG/AST.ag
+cabal exec -- uuagc --haskellsyntax --data --strictdata src/GLua/AG/Token.ag
+cabal exec -- uuagc --catas --haskellsyntax --semfuns --wrappers --signatures src/GLua/AG/PrettyPrint.ag
+cabal exec -- uuagc --catas --haskellsyntax --semfuns --wrappers --signatures --optimize src/GLuaFixer/AG/LexLint.ag
+cabal exec -- uuagc --catas --haskellsyntax --semfuns --wrappers --signatures --optimize src/GLuaFixer/AG/ASTLint.ag
