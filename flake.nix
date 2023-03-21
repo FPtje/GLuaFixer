bae70b2aa96b1c1d91f05d42d7c604d936dbe98d
Falco Peijnenburg <falco.peijnenburg@channable.com>
2023-03-21 22:21:50 +0100

    Reformat flake.nix

--- flake.nix
+++ flake.nix
@@ -12,37 +12,55 @@
       # This override can likely be removed when UUAGC is updated. See
       # https://github.com/UU-ComputerScience/uuagc/pull/11
       uuagc-source = pkgs.fetchFromGitHub
-        { owner = "FPtje";
+        {
+          owner = "FPtje";
           repo = "uuagc";
           rev = "93783365b8e0771b092b9d99264227ca7c5d0823";
           sha256 = "sha256-hvJ/GShwR1AvSofPmmR4FJMPUwUaCMiL1l1CKcKvQw0=";
         };
       haskellOverlay = final: previous: with pkgs.haskell.lib; {
-        uuagc-cabal = final.callCabal2nixWithOptions "uuagc-cabal" uuagc-source "--subpath uuagc/trunk/cabal-plugin" {};
-        uuagc = final.callCabal2nixWithOptions "uuagc-cabal" uuagc-source "--subpath uuagc/trunk" {};
+        uuagc-cabal = final.callCabal2nixWithOptions "uuagc-cabal" uuagc-source "--subpath uuagc/trunk/cabal-plugin" { };
+        uuagc = final.callCabal2nixWithOptions "uuagc-cabal" uuagc-source "--subpath uuagc/trunk" { };
       };
 
       haskellPackages = pkgs.haskell.packages.ghc92.extend haskellOverlay;
       staticHaskellPackages = pkgs.pkgsStatic.haskell.packages.ghc92.extend haskellOverlay;
-    in {
+    in
+    {
       asdf = haskellPackages;
-      packages.glualint = haskellPackages.callPackage ./default.nix {};
+      packages.glualint = haskellPackages.callPackage ./default.nix { };
       # This has to be about the ugliest hack I've ever written. Somehow the overlay in
       # staticHaskellPackages does not apply.
-      packages.glualint-static = (staticHaskellPackages.callPackage ./default.nix {}).override {
-        uuagc = staticHaskellPackages.uuagc.override {uuagc-cabal = staticHaskellPackages.uuagc-cabal;};
+      packages.glualint-static = (staticHaskellPackages.callPackage ./default.nix { }).override {
+        uuagc = staticHaskellPackages.uuagc.override { uuagc-cabal = staticHaskellPackages.uuagc-cabal; };
         uuagc-cabal = staticHaskellPackages.uuagc-cabal;
       };
       defaultPackage = self.packages.${system}.glualint-static;
-      staticHaskellPackages=staticHaskellPackages;
+      staticHaskellPackages = staticHaskellPackages;
 
       devShell = pkgs.mkShell {
         buildInputs = with haskellPackages; [
           cabal-install
           (ghcWithPackages (self: with self; [
-            aeson array base bytestring containers directory filemanip filepath
-            ListLike MissingH mtl optparse-applicative parsec pretty signal
-            uu-parsinglib uuagc uuagc-cabal deepseq
+            aeson
+            array
+            base
+            bytestring
+            containers
+            directory
+            filemanip
+            filepath
+            ListLike
+            MissingH
+            mtl
+            optparse-applicative
+            parsec
+            pretty
+            signal
+            uu-parsinglib
+            uuagc
+            uuagc-cabal
+            deepseq
           ]))
           haskell-language-server
         ];
