19093eafd9f3d7c11a917614599a36281ba673aa
Falco Peijnenburg <falco.peijnenburg@channable.com>
2023-03-21 22:21:12 +0100

    Override UUAGC

--- flake.nix
+++ flake.nix
@@ -8,12 +8,24 @@
   outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
+
+      # This override can likely be removed when UUAGC is updated. See
+      # https://github.com/UU-ComputerScience/uuagc/pull/11
+      uuagc-source = pkgs.fetchFromGitHub
+        { owner = "FPtje";
+          repo = "uuagc";
+          rev = "93783365b8e0771b092b9d99264227ca7c5d0823";
+          sha256 = "sha256-hvJ/GShwR1AvSofPmmR4FJMPUwUaCMiL1l1CKcKvQw0=";
+        };
       haskellOverlay = final: previous: with pkgs.haskell.lib; {
-        uuagc-cabal = markUnbroken previous.uuagc-cabal;
+        uuagc-cabal = final.callCabal2nixWithOptions "uuagc-cabal" uuagc-source "--subpath uuagc/trunk/cabal-plugin" {};
+        uuagc = final.callCabal2nixWithOptions "uuagc-cabal" uuagc-source "--subpath uuagc/trunk" {};
       };
-      haskellPackages = pkgs.haskell.packages.ghc90.extend haskellOverlay;
-      staticHaskellPackages = pkgs.pkgsStatic.haskell.packages.ghc90.extend haskellOverlay;
+
+      haskellPackages = pkgs.haskell.packages.ghc92.extend haskellOverlay;
+      staticHaskellPackages = pkgs.pkgsStatic.haskell.packages.ghc92.extend haskellOverlay;
     in {
+      asdf = haskellPackages;
       packages.glualint = haskellPackages.callPackage ./default.nix {};
       # This has to be about the ugliest hack I've ever written. Somehow the overlay in
       # staticHaskellPackages does not apply.
