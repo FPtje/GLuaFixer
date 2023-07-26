8b1235feadb1d502936544edba781b5a1d7f3b5e
Falco Peijnenburg <falco.peijnenburg@channable.com>
2023-07-26 11:23:38 +0200

    Add test for issue 163

---
+++ tests/golden/data/output/issue-163.lua
@@ -0,0 +1,28 @@
+-- These should not be marked as multiline, because there is only one statement
+-- in the body of each example.
+timer.Create("name", 30, 0, function() if true then return end end)
+timer.Create("name", 30, 0, function() return end)
+timer.Create("name", 30, 0, function() end)
+timer.Create("name", 30, 0)
+func(function() if true then return end end)
+func(function() anything() end)
+-- These, on the other hand, _should_ be marked as multiline
+timer.Create(
+    "name",
+    30,
+    0,
+    function()
+        -- comment here
+        if true then return end
+    end
+)
+
+timer.Create(
+    "name",
+    30,
+    0,
+    function()
+        a = 1
+        return
+    end
+)
