1c3b1a4385447478cff9101775df1617d1c9f142
Falco Peijnenburg <falco.peijnenburg@channable.com>
2023-12-31 16:09:53 +0100

    Have `format: multiline` use the python-style printing

--- tests/golden/data/output/issue-148.lua
+++ tests/golden/data/output/issue-148.lua
@@ -14,21 +14,43 @@ end
 
 -- format: multiline
 local normalSounds = {
-    Sound("honk1.wav"),
-    Sound("honk2.wav"),
-    Sound("honk3.wav"),
-    Sound("honk4.wav")
+    Sound(
+        "honk1.wav"
+    ),
+    Sound(
+        "honk2.wav"
+    ),
+    Sound(
+        "honk3.wav"
+    ),
+    Sound(
+        "honk4.wav"
+    )
 }
 
 -- format: multiline
-render.DrawBeam(self.StartPos - norm, -- Start
-    self.EndPos, -- End
-    64 * (1 - self.Life), -- Width
-    texcoord, -- Start tex coord
-    texcoord + self.Length / 128, -- End tex coord
+render.DrawBeam(
+    self.StartPos - norm,  -- Start
+    self.EndPos,  -- End
+    64 * (1 - self.Life),  -- Width
+    texcoord,  -- Start tex coord
+    texcoord + self.Length / 128,  -- End tex coord
     Color(
-    -- Color (optional)
-    Lerp(self.Life, 255, 50), Lerp(self.Life, 100, 50), 255, 255))
+        -- Color (optional)
+        Lerp(
+            self.Life, 
+            255, 
+            50
+        ), 
+        Lerp(
+            self.Life, 
+            100, 
+            50
+        ), 
+        255, 
+        255
+    )
+)
 
 -- format: multiline
 function a()
@@ -42,23 +64,45 @@ end
 
 --[[ format: multiline ]]
 local normalSounds = {
-    Sound("honk1.wav"),
-    Sound("honk2.wav"),
-    Sound("honk3.wav"),
-    Sound("honk4.wav")
+    Sound(
+        "honk1.wav"
+    ),
+    Sound(
+        "honk2.wav"
+    ),
+    Sound(
+        "honk3.wav"
+    ),
+    Sound(
+        "honk4.wav"
+    )
 }
 
 --[[
 format: multiline
 ]]
-render.DrawBeam(self.StartPos - norm, -- Start
-    self.EndPos, -- End
-    64 * (1 - self.Life), -- Width
-    texcoord, -- Start tex coord
-    texcoord + self.Length / 128, -- End tex coord
+render.DrawBeam(
+    self.StartPos - norm,  -- Start
+    self.EndPos,  -- End
+    64 * (1 - self.Life),  -- Width
+    texcoord,  -- Start tex coord
+    texcoord + self.Length / 128,  -- End tex coord
     Color(
-    -- Color (optional)
-    Lerp(self.Life, 255, 50), Lerp(self.Life, 100, 50), 255, 255))
+        -- Color (optional)
+        Lerp(
+            self.Life, 
+            255, 
+            50
+        ), 
+        Lerp(
+            self.Life, 
+            100, 
+            50
+        ), 
+        255, 
+        255
+    )
+)
 
 local normalSounds = {Sound("honk1.wav"), Sound("honk2.wav"), Sound("honk3.wav"), Sound("honk4.wav")}
 render.DrawBeam(self.StartPos - norm, -- Start
